de127c28b6ee4fd06057858dfb6718ad2c02a024
Hasham Javed <hasham.hasham1@gmail.com>
2021-05-04 20:09:54 +0500

    function for maximum candy added

--- Question4/minMaxCandy.js
+++ Question4/minMaxCandy.js
@@ -7,3 +7,13 @@ function findMinimum(candies, n, k) {
 
   return res;
 }
+function findMaximum(candies, n, k) {
+  let res = 0,
+    index = 0;
+
+  for (let i = n - 1; i >= index; i--) {
+    res += candies[i];
+    index += k;
+  }
+  return res;
+}
